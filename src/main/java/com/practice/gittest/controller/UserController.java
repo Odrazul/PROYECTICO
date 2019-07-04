25cba0cb75952fac9768ba1ae2cbe47d591b7188
nediaz <neiro.diaz@hotmail.com>
2019-07-04 13:33:01 -0500

    ND Commit

---
+++ src/main/java/com/practice/gittest/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.practice.gittest.controller;
+
+public class UserController {
+}
---
+++ src/main/java/com/practice/gittest/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.practice.gittest.repository;
+
+public class UserRepository {
+
+    // Implementations and things like that...
+}
